0565b547f347a6e301ca062da3227a3b4a31b7b2
Anastasia Meshcheryakova <nastya170204@yandex.ru>
2024-09-16 14:05:25 +0700

    lab1

---
+++ JavaConcurrency/lab1/Main.java
@@ -0,0 +1,15 @@
+package lab1;
+public class Main {
+    public static void main (String[] args) {
+        Thread newThread = new Thread(() -> {
+            for (int i = 0; i < 10; i++) {
+                System.out.println("Child " + i);
+            }
+        });
+        newThread.start();
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Parent " + i);
+        }
+    }
+}
