f26a025ac0755b2975511fe4c8fffb120c3e06fb
Anastasia Meshcheryakova <nastya170204@yandex.ru>
2024-09-16 14:11:10 +0700

    changes

--- JavaConcurrency/lab1/Main.java
+++ JavaConcurrency/lab1/Main.java
@@ -3,13 +3,13 @@ public class Main {
     public static void main (String[] args) {
         Thread newThread = new Thread(() -> {
             for (int i = 0; i < 10; i++) {
-                System.out.println("Child " + i);
+                System.out.println("Child " + (i + 1));
             }
         });
         newThread.start();
 
         for (int i = 0; i < 10; i++) {
-            System.out.println("Parent " + i);
+            System.out.println("Parent " + (i + 1));
         }
     }
 }
